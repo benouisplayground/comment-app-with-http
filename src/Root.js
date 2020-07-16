47029edff1fe38e4fdb21199937fba54f97d669a
benouis <benouisplayground@gmail.com>
2020-07-16 20:42:54 +0700

    add test file in actions and reducers

--- src/Root.js
+++ src/Root.js
@@ -3,6 +3,16 @@ import { Provider } from 'react-redux';
 import {createStore} from 'redux';
 import reducers from 'reducers';
 
+
+// to connect react app with redux
+// wrap a App component in a Provider tag then
+// pass a createStore function to a store with
+// a argument of list of reducers and initialize with
+// an empty obj
+
+// to fix the test file. we have to export
+// and root function that wrap a test component with a Provider tag
+
 export default (props) => {
   return (
     <Provider store={createStore(reducers, {})}>
--- src/index.js
+++ src/index.js
@@ -4,12 +4,6 @@ import ReactDOM from 'react-dom';
 import App from 'components/App';
 import Root from 'Root';
 
-// to connect react app with redux
-// wrap a App component in a Provider tag then
-// pass a createStore function to a store with
-// a argument of list of reducers and initialize with
-// an empty obj
-
 ReactDOM.render(
   <Root>
     <App />
