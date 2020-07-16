cc16cfdadd54e95e77948a418cceb3ed890593a0
benouis <benouisplayground@gmail.com>
2020-07-16 19:08:00 +0700

    wire up redux to the application

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore }  from 'redux';
 
 import App from 'components/App';
+import reducers from 'reducers';
 
-ReactDOM.render(<App />,document.querySelector("#root"));
+// to connect react app with redux
+// wrap a App component in a Provider component then
+// pass a createStore function to a store with
+// a argument of list of reducers and initialize with
+// an empty obj
+
+ReactDOM.render(
+  <Provider store={createStore(reducers, {})}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
