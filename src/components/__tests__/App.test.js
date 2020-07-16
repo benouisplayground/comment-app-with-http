a89deafc53511bf91c5783f932184153a552f30f
benouis <benouisplayground@gmail.com>
2020-07-16 15:45:39 +0700

    refactor test file

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -5,14 +5,18 @@ import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
 import { shallow } from 'enzyme';
 
+let wrapped;
+
+beforeEach(()=>{
+  wrapped = shallow(<App />);
+})
+
 it('shows a comment box', () => {
- const wrapped = shallow(<App />);
  
  expect(wrapped.find(CommentBox).length).toEqual(1);
 });
 
 it('shows a comment list', () => {
-  const wrapped = shallow(<App />);
   
   expect(wrapped.find(CommentList).length).toEqual(1);
  });
