f7573310ee377a1cea279f58da68977922db09de
benouis <benouisplayground@gmail.com>
2020-07-16 16:04:51 +0700

    add onChange and onSubmit

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 
 class CommentBox extends Component {
+
+  state = { comment: ''};
+
+  handleChange = (event) => {
+    this.setState({ comment: event.target.value })
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault(); // to prevent the page reload after submiting the form
+
+    // TODO - Call an action creator
+    // and save the comment
+
+    this.setState({ comment: '' });
+  }
+
   render(){
     return(
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <h4>Add a comment</h4>
-        <textarea />
+        <textarea onChange={this.handleChange} value={this.state.comment} />
         <div>
           <button>Submit Comment</button>
         </div>
